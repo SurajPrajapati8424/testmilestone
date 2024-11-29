11ccee85182671235b67e3d683bc06d7dc985c9f
Suraj Prajapati <surajprajapati8286@gmail.com>
2024-11-29 19:47:35 +0530

    SS of hidden & visible using Uint8List-capturedImage

--- lib/screens/screenshotwidgetonly.dart
+++ lib/screens/screenshotwidgetonly.dart
@@ -28,6 +28,38 @@ class ScreenshotWidgetOnlyState extends State<ScreenshotWidgetOnly> {
 
   @override
   Widget build(BuildContext context) {
+    var ss = Container(
+      padding: const EdgeInsets.all(30.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blueAccent, width: 5.0),
+        color: Colors.amberAccent,
+      ),
+      child: Stack(
+        children: [
+          Image.network(
+            'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
+          ),
+          const Text("This widget will be captured as an image"),
+        ],
+      ),
+    );
+    var hiddenContainer = Container(
+        padding: const EdgeInsets.all(30.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.blueAccent, width: 5.0),
+          color: Colors.redAccent,
+        ),
+        child: Stack(
+          children: [
+            Image.network(
+              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
+            ),
+            Text(
+              "This is an invisible widget",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ],
+        ));
     return Scaffold(
       appBar: AppBar(),
       body: Center(
@@ -39,226 +71,25 @@ class ScreenshotWidgetOnlyState extends State<ScreenshotWidgetOnly> {
                   controller: screenshotController,
                   child: Column(
                     children: [
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(30.0),
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.blueAccent, width: 5.0),
-                          color: Colors.amberAccent,
-                        ),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              'https://raw.githubusercontent.com/SachinGanesh/screenshot/refs/heads/master/example/assets/images/flutter.png',
-                            ),
-                            const Text(
-                                "This widget will be captured as an image"),
-                          ],
-                        ),
+                      // Same Multiple times widget
+                      ...[for (int x = 1; x < 12; x++) ss],
+                      ElevatedButton(
+                        child: const Text('Capture An Invisible Widget'),
+                        onPressed: () {
+                          screenshotController
+                              .captureFromWidget(
+                                  InheritedTheme.captureAll(context,
+                                      Material(child: hiddenContainer)),
+                                  delay: const Duration(seconds: 1))
+                              .then((capturedImage) async {
+                            // Save Hidden widget
+                            await saved(
+                                capturedImage: capturedImage,
+                                fileName: 'hidden');
+                            // just for displayings
+                            showCapturedWidget(context, capturedImage);
+                          });
+                        },
                       ),
                     ],
                   )),
@@ -271,12 +102,14 @@ class ScreenshotWidgetOnlyState extends State<ScreenshotWidgetOnly> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          await saved(screenshotController);
           // show
           screenshotController
               .capture(delay: const Duration(milliseconds: 10))
               .then((capturedImage) async {
-            showCapturedWidget(context, capturedImage!);
+            // Save visible image
+            await saved(capturedImage: capturedImage!);
+            // just for displaying
+            showCapturedWidget(context, capturedImage);
           }).catchError((onError) {
             debugPrint(onError);
           });
@@ -301,19 +134,25 @@ class ScreenshotWidgetOnlyState extends State<ScreenshotWidgetOnly> {
   }
 }
 
-Future<void> saved(ScreenshotController screenshotController) async {
+// Future<void> saved(ScreenshotController screenshotController) async {
+Future<void> saved(
+    {required Uint8List capturedImage,
+    String fileName = 'aqScreenshot'}) async {
   // final result = await ImageGallerySaver.save(image.readAsBytesSync());
+  // try {
+  //   await screenshotController.captureAndSave(
+  //     directory.path,
+  //     fileName: filename,
+  //   );
   final directory = Directory('/storage/emulated/0/Download');
   if (!await directory.exists()) {
     await directory.create(recursive: true);
   }
-  String filename = 'aqScreenshot${DateTime.now().millisecondsSinceEpoch}.png';
+  String filename = '$fileName${DateTime.now().millisecondsSinceEpoch}.png';
   try {
-    await screenshotController.captureAndSave(
-      directory.path,
-      fileName: filename,
-    );
-    debugPrint("Saved");
+    final file = File('${directory.path}/$filename');
+    await file.writeAsBytes(capturedImage);
+    debugPrint("Saved at ${file.path}");
   } catch (e) {
     debugPrint("Filed to save Screenshot $e");
   }
